114d0bee561fe406dcbe065ace76c50e24fd9494
Craig <craig.skicko@gmail.com>
2022-08-27 11:42:40 +1000

    Added routes for login and signup pages plus basic handlebars files for testing

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 const homeController = require('./homeController');
 const dashboardController = require('./dashboardController');
+const loginController = require('./loginController');
+const signupController = require('./signupController');
 
 router.use('/', homeController);
 router.use('/dashboard', dashboardController);
+router.use('/login', loginController);
+router.use('/signup', signupController);
 
 module.exports = router;
