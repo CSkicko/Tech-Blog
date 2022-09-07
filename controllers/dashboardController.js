0fa5803c048f267ce4fe3463156acb179d5eef59
Craig <craig.skicko@gmail.com>
2022-09-07 20:33:27 +1000

    Added functionality to display user's posts on dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
+const { User, Post } = require('../models');
 const withAuth = require("../utils/auth");
 
 router.get('/', withAuth, async (req, res) => {
-    res.render('dashboard', { logged_in: req.session.logged_in });
+
+    const userData = await User.findByPk(req.session.user_id, {
+        attributes: [
+            "id",
+            "username"
+        ],
+        include: [
+            {
+                model: Post
+            }
+        ]
+    });
+
+    const userDataClean = userData.get({ plain: true });
+
+    console.log(userDataClean);
+
+    res.render('dashboard', { userDataClean, logged_in: req.session.logged_in });
 });
 
 module.exports = router;
